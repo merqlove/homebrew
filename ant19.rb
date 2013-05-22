a2b54ca65321785daa24b25ea942208159717ab6
Alexander Merkulov <sasha@merqlove.ru>
2013-05-22 14:00:09 +0400

    Fixes

---
+++ ant19.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Ant19 < Formula
+  homepage 'http://ant.apache.org/'
+  url 'http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.0-bin.tar.gz'
+  sha1 '791418e7e80e3f28d6347528e8992a14f09058e7'
+
+  def install
+    rm Dir['bin/*.{bat,cmd,dll,exe}']
+    libexec.install Dir['*']
+
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |f|
+      ln_s f, bin+File.basename(f)
+    end
+  end
+
+  test do
+    system "ant", "-version"
+  end
+end
