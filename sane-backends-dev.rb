dad823a6448b5a1b87602c2e60d80723b97ad4d3
Alexander Merkulov <sasha@merqlove.ru>
2016-07-31 06:15:56 +0400

    Update sane bottles to sha256

--- sane-backends-dev.rb
+++ sane-backends-dev.rb
@@ -3,15 +3,13 @@ require 'formula'
 class SaneBackendsDev < Formula
   homepage 'http://www.sane-project.org/'
   url 'http://anonscm.debian.org/gitweb/?p=sane/sane-backends.git;a=snapshot;h=0fbe145706b954d5aede360c4c9e703e6178212e;sf=tgz'
-  sha1 '49f62868569edfccbfc8de70e87996c94739a176'
+  sha256 'ad392da514e11da017b572edf2f910df49e86c8ee890267e49f2902d1d991681'
 
   # option :universal
   bottle do
     root_url 'http://content.merqlove.ru/bottles'
-    # sha1 'f4307ebd1fe094dbd14e4e19c717baa83bdd9631' => :snow_leopard
-    # sha1 '45d35923b0439617adb86630bdd4985a6cf03984' => :lion
-    sha1 'a3034eab8f1898afcecb72d9de41025616b56b5a' => :mountain_lion
-    sha1 'f0e9337b186fa66298ec491878c46cf441848d92' => :mavericks
+    sha256 'c3feb00518297576af62fbb3dad207363f046d61d7f971d66d7f98f166e37a9e' => :mountain_lion
+    sha256 '3d732c3c05207ad1406f0c740c2c9ccafb6137468f114c6d60787f3f1cba217e' => :mavericks
   end
 
   depends_on 'jpeg'
