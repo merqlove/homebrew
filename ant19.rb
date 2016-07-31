c400be8fa9777eaa029fe0b0213f969d3da31022
Alexander Merkulov <sasha@merqlove.ru>
2016-07-31 06:17:49 +0400

    Ant to SHA256

--- ant19.rb
+++ ant19.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ant19 < Formula
   homepage 'http://ant.apache.org/'
   url 'http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.0-bin.tar.gz'
-  sha1 '791418e7e80e3f28d6347528e8992a14f09058e7'
+  sha256 'd79dd4961a508d41618c98c3f1871d6c1eb1372f70b79439ba9c0e8c3ddc7e5f'
 
   def install
     rm Dir['bin/*.{bat,cmd,dll,exe}']
