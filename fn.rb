3dedfdc4322c31ad71a5d1ce51f9d3fe09561699
Alexander Merkulov <sasha@merqlove.ru>
2017-09-09 00:11:18 +0300

    Cleanup fn

--- fn.rb
+++ fn.rb
@@ -1,16 +1,14 @@
 class Fn < Formula
-  desc "Go version of the Iron.io Functions command-line tools"
+  desc "IronFunctions command line tools"
   homepage "https://iron.io"
   url "https://github.com/iron-io/functions/releases/download/0.2.62/fn_mac"
   sha256 "ba753fbaa4f9487c99f9fa5b870e8118ec2d8a558735ed3ce1144a19fda92460"
 
-  bottle :unneeded
-
   def install
-    bin.install "fn_mac"
-    system "mv", "#{bin}/fn_mac", "#{bin}/fn"
+    mv "#{bin}/fn_mac", "#{bin}/fn"
+    bin.install "fn"
   end
-  
+
   test do
     system "#{bin}/fn", "-help"
   end
