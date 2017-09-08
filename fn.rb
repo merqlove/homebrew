e216155e50c7c72e41f43241c7fc3e159072ca2f
Alexander Merkulov <sasha@merqlove.ru>
2017-09-09 00:51:48 +0300

    Update fn.rb

--- fn.rb
+++ fn.rb
@@ -1,10 +1,10 @@
 class Fn < Formula
   desc "IronFunctions command-line tools"
   homepage "https://iron.io"
-  
+
   stable do
     url "https://github.com/iron-io/functions/releases/download/0.2.62/fn_mac"
-    sha256 "ba753fbaa4f9487c99f9fa5b870e8118ec2d8a558735ed3ce1144a19fda92460"    
+    sha256 "ba753fbaa4f9487c99f9fa5b870e8118ec2d8a558735ed3ce1144a19fda92460"
     version "0.2.62"
   end
 
