b2392b7f3dfa0ce8c94f091b2b55c7c2cc136e92
Alexander Merkulov <sasha@merqlove.ru>
2017-09-09 02:29:15 +0300

    Update fn.rb

--- fn.rb
+++ fn.rb
@@ -5,15 +5,16 @@ class Fn < Formula
   stable do
     url "https://github.com/iron-io/functions/releases/download/0.2.62/fn_mac"
     sha256 "ba753fbaa4f9487c99f9fa5b870e8118ec2d8a558735ed3ce1144a19fda92460"
-    version "0.2.62-11"
+    version "0.2.62-12"
   end
 
   bottle :unneeded
 
   def install
-    prefix.install "fn_mac", "#{prefix}/fn"
-    puts system "ls -all"
-    bin.install "#{prefix}/fn"
+    libexec.install "fn_mac"
+    FileUtils.mv libexec/"fn_mac", libexec/"fn"
+    FileUtils.chmod "+x", libexec/"fn"
+    bin.install_symlink libexec/"fn"
   end
 
   test do
